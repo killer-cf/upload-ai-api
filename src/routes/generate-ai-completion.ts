e017342b387e66340f15eb5ca9f2c04b8fea7e61
killer-cf <costa.kilder@live.com>
2023-09-14 13:50:49 -0300

    add seed and refactor generate ai completions

--- src/routes/generate-ai-completion.ts
+++ src/routes/generate-ai-completion.ts
@@ -1,5 +1,5 @@
 import { FastifyInstance } from "fastify";
-import { createReadStream } from 'node:fs'
+import { streamToResponse, OpenAIStream } from 'ai'
 import { prisma } from "../lib/prisma";
 import { openai } from "../lib/openai";
 import z from 'zod'
@@ -31,9 +31,17 @@ export async function generateAiCompletionRoute(app: FastifyInstance) {
       temperature,
       messages: [
         { role: 'user', content: promptMessage }
-      ]
+      ],
+      stream: true
     })
 
-    return response
+    const stream = OpenAIStream(response)
+
+    streamToResponse(stream, reply.raw, {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      }
+    })
   })
 }
